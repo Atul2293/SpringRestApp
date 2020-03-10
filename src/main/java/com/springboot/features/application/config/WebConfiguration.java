fffd7a9f25ed24ec0bfd7ad7febc12a4c586ee4a
Atul Mehetre <atul.mehetre@virginactive.co.za>
2020-03-10 22:21:59 +0530

    Added Spring Security Feature using JWT Tokens

---
+++ src/main/java/com/springboot/features/application/config/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.springboot.features.application.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Used for H2 DB path config
+ */
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
+}
---
+++ src/main/java/com/springboot/features/application/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.springboot.features.application.dao;
+
+import com.springboot.features.application.model.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+    DAOUser findByUsername(String username);
+}
